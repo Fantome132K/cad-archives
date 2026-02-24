97ca8bf9cad2dfb8b48a0dd7e265183884a0bc91
Fantome132k <fantome132k@gmail.com>
2026-02-24 19:57:33 +0100

    feat: nettoyage automatique des fichiers expires

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from database import Base, engine
 from route import auth, upload, files
+from apscheduler.schedulers.background import BackgroundScheduler
+from cleanup import delete_expired_files
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(delete_expired_files, "interval", hours=1)
+scheduler.start()
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(upload.router, prefix="/files")
